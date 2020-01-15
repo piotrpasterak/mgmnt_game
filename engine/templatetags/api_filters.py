190336a1d68db705a80aaf7045328c0dfe72e949
Jakub Rak <jakub.r.rak@gmail.com>
2020-01-16 00:10:58 +0100

    all steps are finished

--- engine/templatetags/api_filters.py
+++ engine/templatetags/api_filters.py
@@ -5,3 +5,10 @@ register = template.Library()
 @register.filter
 def get_at_index(list, index):
     return list[index]
+
+@register.inclusion_tag('game/basic_game_form.html')
+def game_form(round_data, round_iterator, checkboxes=[]):
+    context = {'round_data': round_data,
+        'round_iterator': round_iterator,
+        'checkboxes': checkboxes}
+    return context
--- engine/views.py
+++ engine/views.py
@@ -5,6 +5,7 @@ from json import loads
 from django.http import HttpResponseBadRequest, JsonResponse
 from django.views.generic import TemplateView
 
+from process.models import Round, Game
 from .tools.game_engine import GameEngine, RoundEngine
 
 class PostTemplateView(TemplateView):
@@ -13,8 +14,12 @@ class PostTemplateView(TemplateView):
         return HttpResponseBadRequest('Bad GET request!')
 
     def post(self, request, *args, **kwargs):
-        print(request)
-        print(request.POST)
+        p = dict(request.POST)
+        for key in p.keys():
+            if type(p[key]) is list:
+                p[key] = p[key][0]
+        self.post = p
+
         context = self.get_context_data()
         rendered = self.render_to_response(context)
         return rendered
@@ -35,7 +40,7 @@ class InitGame(PostTemplateView):
         r = re.init_round()
         context['round'] = r
         context['round_data'] = loads(r.possibilities)
-        context['round_iterator'] = list(range(1, 1+context['round_data']['projekty']))
+        context['round_iterator'] = list(range(context['round_data']['projekty']))
 
         return context
 
@@ -46,10 +51,29 @@ class RoundSubmit(PostTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(self.request)
-        print(self.request.__dict__)
-        print(self.request.POST)
-        print(dict(self.request.POST))
+        print(self.post)
+
+        rd = Round.objects.get(pk=self.post['roundId'])
+        context['round'] = rd
+        context['round_data'] = loads(rd.possibilities)
+        context['round_iterator'] = list(range(context['round_data']['projekty']))
+        context['game_id'] = rd.game_id
+
+        self.add_checkboxes_to_context(context)
+
+        return context
+
+    def add_checkboxes_to_context(self, context):
+        checkboxes = []
+        for itr in range(context['round_data']['projekty']):
+            name = 'project' + str(itr)
+            if name in self.post:
+                checkboxes.append(" checked")
+            else:
+                checkboxes.append(" ")
+
+        context['checkboxes'] = checkboxes
+
         return context
 
 
@@ -59,6 +83,17 @@ class InitRound(PostTemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        g = Game.objects.get(id=self.post['gameId'])
+        context['game'] = g
+        context['game_id'] = g.id
+
+        re = RoundEngine(context['game'])
+        r = re.init_round()
+        context['round'] = r
+        context['round_data'] = loads(r.possibilities)
+        context['round_iterator'] = list(range(context['round_data']['projekty']))
+
         return context
 
 class ProjectView(PostTemplateView):
